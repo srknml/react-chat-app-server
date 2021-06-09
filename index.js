aba821df37e1ab524bc9ecb643f54592050b2d1a
Schude <nemli.serkan2@gmail.com>
2021-06-10 00:34:18 +0300

    env PORT

--- index.js
+++ index.js
@@ -40,6 +40,4 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(5000, () => {
-    console.log('listening on port 5000');
-});
+app.listen(process.env.PORT || 5000, () => console.log('Server is running...'));
