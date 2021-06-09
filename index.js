3b25212e0dcfa84afc45b6dd826f7bd105713949
Schude <nemli.serkan2@gmail.com>
2021-06-10 01:05:56 +0300

    engines

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
 const app = express();
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-    cors: {
-        origin: '*',
-    },
-});
+const io = socketio(server);
+
+app.use(cors());
 
 let users = [];
 io.on('connection', (socket) => {
@@ -40,9 +41,8 @@ io.on('connection', (socket) => {
         io.emit('users', users);
     });
 });
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 server.listen(server_port, server_host, function () {
     console.log('Listening on port %d', server_port);
 });
-// server.listen(process.env.PORT || 5000, () => console.log('Server is running...'));
