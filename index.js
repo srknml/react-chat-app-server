ba152faefc3a032be53a374a81e3bddae61d49f2
Schude <nemli.serkan2@gmail.com>
2021-06-10 17:26:53 +0300

    header

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const express = require('express');
 const socketio = require('socket.io');
 
 const app = express();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 const server = https.createServer(app);
 
 // const io = socketio(server, {
